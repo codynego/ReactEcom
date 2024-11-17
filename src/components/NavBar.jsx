0fdf0fe50d5526cd296cf7a0c13d01ca290eb431
abednego emonena <emonenaabednego@gmail.com>
2024-11-17 17:12:01 +0100

    initialize React and Vite project with Tailwind CSS setup

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const NavBar = () => {
+  return (
+    <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">hello</nav>
+    </div>
+  )
+}
